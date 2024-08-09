4221db500c84b437ef0d6fe2e7b25ec207f00007
mikemaslo <mike.maslo96@gmail.com>
2024-08-09 16:40:57 +0300

    Chapter 13 Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from '@/app/ui/invoices/edit-form'
 import {fetchCustomers, fetchInvoiceById} from "@/app/lib/data";
+import {notFound} from "next/navigation";
 
 export default async function Page(
     {params}: {
@@ -13,6 +14,10 @@ export default async function Page(
         fetchInvoiceById(id),
         fetchCustomers()
     ])
+
+    if (!invoice) {
+        notFound()
+    }
 
     return (
         <main>
